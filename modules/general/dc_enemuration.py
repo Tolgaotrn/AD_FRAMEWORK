dff4538ab5b36ed5d0bc11ef1b49994b181a175a
tolga <tolgaaoturan@gmail.com>
2025-03-21 03:21:46 +0100

    dc enemuration

--- modules/general/dc_enemuration.py
+++ modules/general/dc_enemuration.py
@@ -5,12 +5,12 @@ def find_dc_ip():
 
     target_domain = input("Please provide a domain address: ")
     if target_domain:
-        command = f'nslookup -type-SRV _ldap._tcp.dc._msdcs.{target_domain}'
+        command = f'nslookup -type=SRV _ldap._tcp.dc._msdcs.{target_domain}'
     else:
         exit(1)
 
 
-        
+
     print(f'\n[+] Running command: {command}')
 
     try :
